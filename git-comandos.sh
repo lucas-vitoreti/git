7cb8360b40fa28fbc8421cc44cf41526fdfaf8ce
lucas <vitoreti.code@gmail.com>
2021-03-06 17:11:12 -0300

    add comands

--- git-comandos.sh
+++ git-comandos.sh
@@ -29,8 +29,24 @@ git config user.name
 # vendo o historico em uma linha
 git log --oneline
 
-# vendo historico detalhado
+# vendo historico detalhado da modificação
 git log -p
 
 # git log cheatsheet
 https://devhints.io/git-log
+
+# listar repositorios remotos
+git remote
+
+# add repositorio remoto
+git remote add local C:/Users/c82414a/servidor
+
+# criar repositorio git vazio
+git init --bare
+
+# renomear repostiorio
+git remote rename origin local
+
+# git pull de um repo e uma branch especifica
+git pull local master
+
